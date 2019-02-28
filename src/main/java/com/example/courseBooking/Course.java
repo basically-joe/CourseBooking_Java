688a20f97ebc68392e1df5c4ed898d979843b536
Davide Venditti <davide_335@msn.com>
2019-02-28 11:37:40 +0000

    add course

---
+++ src/main/java/com/example/courseBooking/Course.java
@@ -0,0 +1,72 @@
+package com.example.courseBooking;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String town;
+    private double rating;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "course")
+    private List<Booking> bookings;
+
+
+    public Course(String name, String town, double rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
