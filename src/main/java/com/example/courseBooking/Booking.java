bd15272fd2648333926ba1c01c296080603d4e1c
Davide Venditti <davide_335@msn.com>
2019-02-28 11:51:29 +0000

    add getter and setter for booking

--- src/main/java/com/example/courseBooking/Booking.java
+++ src/main/java/com/example/courseBooking/Booking.java
@@ -31,5 +31,35 @@ public class Booking {
         this.customer = customer;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
